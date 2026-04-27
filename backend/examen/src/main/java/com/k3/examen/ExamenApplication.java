8e4af4366a60f544f638b14795ff612ef6c13ed4
Fiononantsoa01 <andrifefyh@gmail.com>
2026-04-27 08:29:14 +0200

    database structure and backend structure

---
+++ backend/examen/src/main/java/com/k3/examen/ExamenApplication.java
@@ -0,0 +1,13 @@
+package com.k3.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+
+}
